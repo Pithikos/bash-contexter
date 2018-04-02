5882339a1ebfd90279c330e7f6615af5081da215
Manos S.H <manossef@gmail.com>
2018-04-02 18:57:15 +0100

    Update bash_contexter.sh after removal of python

--- bash_contexter.sh
+++ bash_contexter.sh
@@ -1,13 +1,12 @@
-#
-# Needed by multiple other modules
-#
-
-
-# Usage
-# 1. Use register_command_not_found to register a function to fallback to
-
 BASH_CONTEXTER_PATH=~/.bash_contexter
-BASH_CONTEXTER_PY="$BASH_CONTEXTER_PATH/bash_contexter.py"
+
+# Warn user of double sourcing
+if type _before_bash_contexter_command_not_found_handle &>/dev/null; then
+	echo "WARNING: Seems you already sourced this. This can lead to infinite loops.."
+fi
+
+declare -A _bash_contexter_prechecks_commands
+declare -a _bash_contexter_context_commands
 
 
 _is_shell_bash () {
@@ -27,43 +26,60 @@ if ! _is_shell_bash; then
   echo "This script is only supported for Bash v4.4+"
 fi
 
+# Store any pre-defined command_not_found_handle
+if type command_not_found_handle &> /dev/null; then
+  eval "_before_bash_contexter_$(declare -f command_not_found_handle)"
+fi
 
-# Store existing command_not_found_handle if it has contents or else use the default
+
+# Default handle when no pre-defined command_not_found_handle is used
 _default_command_not_found_handle () {
   bash -c "$@"
   return $?
 }
-if type command_not_found_handle &> /dev/null; then
-  eval "_before_bash_contexter_$(declare -f command_not_found_handle)"
-	python -B "$BASH_CONTEXTER_PY" --register "_before_bash_contexter_command_not_found_handle"
-else
-	python -B "$BASH_CONTEXTER_PY" --register "_default_command_not_found_handle"
-fi
+
+
+command_not_found_handle () {
+	for (( i=${#_bash_contexter_context_commands[@]}-1 ; i>=0 ; i-- )) ; do
+		cmd="${_bash_contexter_context_commands[i]}"
+
+		# Go through prechecks and execute context functions
+		if ${_bash_contexter_prechecks_commands[$cmd]}; then
+			$cmd "$@"
+			return $?
+		fi
+	done
+
+	# Fallback to _before_bash_contexter_command_not_found_handle
+	if type _before_bash_contexter_command_not_found_handle &> /dev/null; then
+		echo "running before_"
+		_before_bash_contexter_command_not_found_handle "$@"
+		return $?
+	fi
+
+	# Fallback to _default_command_not_found_handle
+	echo "running default_"
+	_default_command_not_found_handle "$@"
+	return $?
+}
 
 
 # ------------------------------------ API -------------------------------------
 
 
 bash_contexter_register () {
-	# Register and export to python wrapper
-  python "$BASH_CONTEXTER_PY" --register "$1"
-	export -f "$1"
 
-	# Check if precheck exists.
-	if type "precheck_$1" &> /dev/null; then
-		python -B "$BASH_CONTEXTER_PY" --register-precheck "$1" "precheck_$1"
-		export -f "precheck_$1"
+	# Register precheck first since it's a dependancy
+	if ! type "precheck_$1" &> /dev/null; then
+		echo "No precheck found for $1, will not register it."
+		return 1
 	else
-		echo "No precheck found for $1, continuing without."
+		_bash_contexter_prechecks_commands["$1"]="precheck_$1"
 	fi
-}
 
-echo "Instantiate command_not_found_handle"
-command_not_found_handle () {
-	echo "Running through python contexter"
-	export -f "echo_command"
-	python -c "from subprocess import check_output; print(check_output('echo_command tsfsd', shell=True))"
-	# python -B "$BASH_CONTEXTER_PY" --run "$@"
+	# Register command
+	_bash_contexter_context_commands+=("$1")
+
 }
 
 
