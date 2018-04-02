613febbf2ff8e85fa690eed45f170061d73ce2a2
Manos S.H <manossef@gmail.com>
2018-04-02 19:58:17 +0100

    Add docker example

---
+++ examples/docker.sh
@@ -0,0 +1,29 @@
+# Check if a docker container is running
+function _docker_container_is_running {
+	running=$(docker inspect --format="{{.State.Running}}" $1 2>/dev/null)
+	if [[ $running != "true" ]]; then
+		false
+	else
+		true
+	fi
+}
+
+
+function docker_autoenter {
+	if [ $2 ]; then
+		docker exec -it "$@"
+	else
+		docker exec -it $1 bash
+	fi
+}
+
+
+function precheck_docker_autoenter {
+	if _docker_container_is_running $1; then
+		return 0
+	fi
+	return 1
+}
+
+
+bash_contexter_register 'docker_autoenter'
